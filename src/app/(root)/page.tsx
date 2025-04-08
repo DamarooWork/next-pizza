558345e52df8503631a11ff0b1f7aeecc09e01cb
Damaroo <levkichigin@yandex.ru>
2025-04-08 09:09:01 +0300

    fix: deploy vercel

--- src/app/(root)/page.tsx
+++ src/app/(root)/page.tsx
@@ -2,27 +2,35 @@ import { Container, Filters, Title, TopBar } from '@/components/shared'
 import { ProductsGroupList } from '@/components/shared'
 import { Suspense } from 'react'
 import { prisma } from '@/../prisma'
+import { Category } from '@prisma/client'
 export default async function Home() {
-  const categories = await prisma?.category.findMany({
-    include: {
-      products: {
-        include: {
-          items: true,
-          ingredients: true,
+  let categories
+  try {
+    categories = await prisma?.category.findMany({
+      include: {
+        products: {
+          include: {
+            items: true,
+            ingredients: true,
+          },
         },
       },
-    },
-  })
+    })
+  } catch (error) {
+    console.log(error)
+  }
   return (
     <>
       <Container className="mt-4 lg:mt-10">
         <Title text="Все пиццы" size="lg" className="font-extrabold" />
       </Container>
-      <TopBar
-        categories={categories?.filter(
-          (category) => category?.products.length > 0
-        )}
-      />
+      {categories && (
+        <TopBar
+          categories={categories?.filter(
+            (category) => category?.products.length > 0
+          )}
+        />
+      )}
       <Container className="mt-10 pb-14 ">
         <div className="flex  gap-[60px]  ">
           {/* Фильтрация */}
@@ -31,21 +39,23 @@ export default async function Home() {
           </Suspense>
 
           {/* Список товаров */}
-          <div className="flex-1">
-            <div className="flex flex-col gap-16">
-              {categories?.map(
-                (category, i) =>
-                  category?.products.length > 0 && (
-                    <ProductsGroupList
-                      key={category.id}
-                      title={category.name}
-                      items={category.products}
-                      categoryId={category.id}
-                    />
-                  )
-              )}
+          {categories && (
+            <div className="flex-1">
+              <div className="flex flex-col gap-16">
+                {categories?.map(
+                  (category, i) =>
+                    category?.products.length > 0 && (
+                      <ProductsGroupList
+                        key={category.id}
+                        title={category.name}
+                        items={category.products}
+                        categoryId={category.id}
+                      />
+                    )
+                )}
+              </div>
             </div>
-          </div>
+          )}
         </div>
       </Container>
     </>
