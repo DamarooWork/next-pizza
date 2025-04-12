fa6f915550c82fb405e36e1faadff9aea9918e16
Damaroo <levkichigin@yandex.ru>
2025-04-12 14:12:46 +0300

    feat(cart): image when no cartitems

--- src/components/shared/cart-drawer.tsx
+++ src/components/shared/cart-drawer.tsx
@@ -10,7 +10,7 @@ import {
   SheetTrigger,
 } from '@/components/ui/sheet'
 import { Button } from '../ui'
-import { ArrowRight } from 'lucide-react'
+import { ArrowLeft, ArrowRight } from 'lucide-react'
 import Link from 'next/link'
 import { CartDrawerItem } from './cart-drawer-item'
 import { getCartItemDetails } from '@/lib/get-cart-item-details'
@@ -18,6 +18,9 @@ import { useCartStore } from '@/store'
 import { useEffect, useState } from 'react'
 import { PizzaSize, PizzaType } from '@/lib/constants'
 import { getDeclension } from '@/lib/utils'
+import Image from 'next/image'
+import { Title } from './title'
+import { DialogTitle } from '@radix-ui/react-dialog'
 
 interface Props {
   className?: string
@@ -41,72 +44,101 @@ export function CartDrawer({ children }: Props) {
     <Sheet>
       <SheetTrigger asChild>{children}</SheetTrigger>
       <SheetContent className="flex flex-col justify-between pb-0 bg-[#f4f1ee]">
-        <SheetHeader>
-          <SheetTitle>
-            В корзине{' '}
-            <span className="font-bold">
-              {items.length}{' '}
-              {getDeclension(items.length, {
-                one: 'товар',
-                few: 'товара',
-                many: 'товаров',
-              })}
-            </span>
-          </SheetTitle>
-        </SheetHeader>
-
-        {/* Cart items */}
-        <div className="mt-5 overflow-auto flex-1 flex flex-col gap-2">
-          {items.map((item) => (
-            <CartDrawerItem
-              key={item.id}
-              id={item.id}
-              imageUrl={item.imageUrl}
-              details={
-                item.pizzaSize && item.pizzaType
-                  ? getCartItemDetails(
-                      item.ingredients,
-                      item.pizzaSize as PizzaSize,
-                      item.pizzaType as PizzaType
+        {totalAmount > 0 ? (
+          <>
+            {' '}
+            <SheetHeader>
+              <SheetTitle>
+                В корзине{' '}
+                <span className="font-bold">
+                  {items.length}{' '}
+                  {getDeclension(items.length, {
+                    one: 'товар',
+                    few: 'товара',
+                    many: 'товаров',
+                  })}
+                </span>
+              </SheetTitle>
+            </SheetHeader>
+            {/* Cart items */}
+            <div className="mt-5 overflow-auto flex-1 flex flex-col gap-2">
+              {items.map((item) => (
+                <CartDrawerItem
+                  key={item.id}
+                  id={item.id}
+                  imageUrl={item.imageUrl}
+                  details={
+                    item.pizzaSize && item.pizzaType
+                      ? getCartItemDetails(
+                          item.ingredients,
+                          item.pizzaSize as PizzaSize,
+                          item.pizzaType as PizzaType
+                        )
+                      : ''
+                  }
+                  disabled={item.disabled}
+                  name={item.name}
+                  price={item.price}
+                  quantity={item.quantity}
+                  onClickCountButton={(type) =>
+                    updateItemQuantity(
+                      item.id,
+                      type === 'plus' ? item.quantity + 1 : item.quantity - 1
                     )
-                  : ''
-              }
-              name={item.name}
-              price={item.price}
-              quantity={item.quantity}
-              onClickCountButton={(type) =>
-                updateItemQuantity(
-                  item.id,
-                  type === 'plus' ? item.quantity + 1 : item.quantity - 1
-                )
-              }
-              onClickRemove={() => removeCartItem(item.id)}
+                  }
+                  onClickRemove={() => removeCartItem(item.id)}
+                />
+              ))}
+            </div>
+            <SheetFooter className="bg-white p-8 ">
+              <div className="flex mb-4">
+                <span className="flex flex-1 text-lg text-neutral-500">
+                  Итого
+                  <div className="flex-1 border-b border-dashed border-b-neutral-200 relative -top-1 mx-2" />
+                </span>
+
+                <span className="font-bold text-lg">{totalAmount} ₽</span>
+              </div>
+
+              <Link href="/checkout">
+                <Button
+                  onClick={() => setRedirecting(true)}
+                  loading={redirecting}
+                  type="submit"
+                  className="w-full h-12 text-base"
+                >
+                  Оформить заказ
+                  <ArrowRight className="w-5 ml-2" />
+                </Button>
+              </Link>
+            </SheetFooter>
+          </>
+        ) : (
+          <div className="flex flex-col justify-between items-center my-auto">
+            <DialogTitle className="hidden">Корзина пустая</DialogTitle>
+            <Image
+              src="/assets/images/empty-box.png"
+              alt="Корзина"
+              width={150}
+              height={150}
             />
-          ))}
-        </div>
+            <Title
+              size="md"
+              text="Корзина пустая"
+              className="text-center font-bold my-2"
+            />
+            <p className="text-center text-neutral-500 mb-5 px-4">
+              Добавьте хотя бы одну пиццу, чтобы совершить заказ
+            </p>
 
-        <SheetFooter className="bg-white p-8 ">
-          <div className="flex mb-4">
-            <span className="flex flex-1 text-lg text-neutral-500">
-              Итого
-              <div className="flex-1 border-b border-dashed border-b-neutral-200 relative -top-1 mx-2" />
-            </span>
-
-            <span className="font-bold text-lg">{totalAmount} ₽</span>
+            <SheetClose>
+              <Button className="w-56 h-12 text-base" size="lg">
+                <ArrowLeft className="w-5 mr-2" />
+                Вернуться назад
+              </Button>
+            </SheetClose>
           </div>
-
-          <Link href="/checkout">
-            <Button
-              onClick={() => setRedirecting(true)}
-              loading={redirecting}
-              type="submit"
-              className="w-full h-12 text-base"
-            >
-              Оформить заказ
-              <ArrowRight className="w-5 ml-2" />
-            </Button>
-          </Link>
-        </SheetFooter>
+        )}
       </SheetContent>
     </Sheet>
   )
--- src/store/cart.ts
+++ src/store/cart.ts
@@ -1,27 +1,27 @@
-import { create } from 'zustand';
-import { Api } from '../services/api-client';
-import { CartStateItem, getCartDetails } from '@/lib/get-cart-details';
-import { CreateCartItemValues } from '../services/dto/cart.dto';
+import { create } from 'zustand'
+import { Api } from '../services/api-client'
+import { CartStateItem, getCartDetails } from '@/lib/get-cart-details'
+import { CreateCartItemValues } from '../services/dto/cart.dto'
 
 export interface CartState {
-  loading: boolean;
-  error: boolean;
-  totalAmount: number;
-  items: CartStateItem[];
+  loading: boolean
+  error: boolean
+  totalAmount: number
+  items: CartStateItem[]
 
   /* Получение товаров из корзины */
-  fetchCartItems: () => Promise<void>;
+  fetchCartItems: () => Promise<void>
 
   /* Запрос на обновление количества товара */
-  updateItemQuantity: (id: number, quantity: number) => Promise<void>;
+  updateItemQuantity: (id: number, quantity: number) => Promise<void>
 
   /* Запрос на добавление товара в корзину */
-  addCartItem: (values: CreateCartItemValues) => Promise<void>;
+  addCartItem: (values: CreateCartItemValues) => Promise<void>
 
   /* Запрос на удаление товара из корзины */
-  removeCartItem: (id: number) => Promise<void>;
+  removeCartItem: (id: number) => Promise<void>
 }
- 
+
 export const useCartStore = create<CartState>((set, get) => ({
   items: [],
   error: false,
@@ -30,27 +30,38 @@ export const useCartStore = create<CartState>((set, get) => ({
 
   fetchCartItems: async () => {
     try {
-      set({ loading: true, error: false });
-      const data = await Api.cart.getCart();
-      set(getCartDetails(data));
+      set({ loading: true, error: false })
+      const data = await Api.cart.getCart()
+      set(getCartDetails(data))
     } catch (error) {
-      console.error(error);
-      set({ error: true });
+      console.error(error)
+      set({ error: true })
     } finally {
-      set({ loading: false });
+      set({ loading: false })
     }
   },
 
   updateItemQuantity: async (id: number, quantity: number) => {
     try {
-      set({ loading: true, error: false });
-      const data = await Api.cart.updateItemQuantity(id, quantity);
-      set(getCartDetails(data));
+      set((state) => ({
+        loading: true,
+        error: false,
+        items: state.items.map((item) =>
+          item.id === id ? { ...item, disabled: true } : item
+        ),
+      }))
+      const data = await Api.cart.updateItemQuantity(id, quantity)
+      set(getCartDetails(data))
     } catch (error) {
-      console.error(error);
-      set({ error: true });
+      console.error(error)
+      set({ error: true })
     } finally {
-      set({ loading: false });
+      set((state) => ({
+        loading: false,
+        items: state.items.map((item) =>
+          item.id === id ? { ...item, disabled: false } : item
+        ),
+      }))
     }
   },
 
@@ -59,31 +70,35 @@ export const useCartStore = create<CartState>((set, get) => ({
       set((state) => ({
         loading: true,
         error: false,
-        items: state.items.map((item) => (item.id === id ? { ...item, disabled: true } : item)),
-      }));
-      const data = await Api.cart.removeCartItem(id);
-      set(getCartDetails(data));
+        items: state.items.map((item) =>
+          item.id === id ? { ...item, disabled: true } : item
+        ),
+      }))
+      const data = await Api.cart.removeCartItem(id)
+      set(getCartDetails(data))
     } catch (error) {
-      console.error(error);
-      set({ error: true });
+      console.error(error)
+      set({ error: true })
     } finally {
       set((state) => ({
         loading: false,
-        items: state.items.map((item) => ({ ...item, disabled: false })),
-      }));
+        items: state.items.map((item) =>
+          item.id === id ? { ...item, disabled: false } : item
+        ),
+      }))
     }
   },
 
   addCartItem: async (values: CreateCartItemValues) => {
     try {
-      set({ loading: true, error: false });
-      const data = await Api.cart.addCartItem(values);
-      set(getCartDetails(data));
+      set({ loading: true, error: false })
+      const data = await Api.cart.addCartItem(values)
+      set(getCartDetails(data))
     } catch (error) {
-      console.error(error);
-      set({ error: true });
+      console.error(error)
+      set({ error: true })
     } finally {
-      set({ loading: false });
+      set({ loading: false })
     }
   },
-}));
+}))
